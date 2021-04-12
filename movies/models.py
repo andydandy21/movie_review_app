474dfe439e77bc086c5f45ff9f6e3db567364421
Andrew Smith <a.smithb21@gmail.com>
2021-04-12 11:41:17 -0500

    adjusted models for blank and null fields

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
@@ -6,10 +7,11 @@ from django.utils import timezone
 
 class Crew(models.Model):
     
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, verbose_name='Name')
-    birth_date = models.DateField(verbose_name='Birthday')
-    birth_place = models.CharField(max_length=200, verbose_name='Birthplace')
-    biography = models.TextField(max_length=5000, verbose_name='Bio')
+    birth_date = models.DateField(blank=True, null=True, verbose_name='Birthday')
+    birth_place = models.CharField(max_length=200, blank=True, verbose_name='Birthplace')
+    biography = models.TextField(max_length=5000, blank=True, verbose_name='Bio')
 
     def __str__(self):
 
@@ -17,6 +19,7 @@ class Crew(models.Model):
 
 class Genre(models.Model):
     
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     genre = models.CharField(max_length=200, unique=True, verbose_name='Genre')
 
     def __str__(self):
@@ -25,15 +28,16 @@ class Genre(models.Model):
 
 class Movie(models.Model):
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200, verbose_name='Title')
-    production_company = models.CharField(max_length=200, verbose_name='Production Company')
-    distribution_company = models.CharField(max_length=200, verbose_name='Distributed By')
-    date_released = models.DateField(verbose_name='Release Date')
-    genre = models.ManyToManyField(Genre, related_name='movies', verbose_name='Genre')
-    movie_rated = models.CharField(max_length=5, verbose_name='Rated')
-    movie_length = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(51420)], verbose_name='Running Time')
-    plot = models.TextField(max_length=5000, verbose_name='Plot')
-    crew = models.ManyToManyField(Crew, related_name='movies', verbose_name='Cast and Crew')
+    production_company = models.CharField(max_length=200, blank=True, verbose_name='Production Company')
+    distribution_company = models.CharField(max_length=200, blank=True, verbose_name='Distributed By')
+    date_released = models.DateField(default=None, blank=True, null=True, verbose_name='Release Date')
+    genre = models.ManyToManyField(Genre, blank=True, null=True, related_name='movies', verbose_name='Genre')
+    movie_rated = models.CharField(max_length=5, blank=True, verbose_name='Rated')
+    movie_length = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(51420)], blank=True, null=True, verbose_name='Running Time')
+    plot = models.TextField(max_length=5000, blank=True, verbose_name='Plot')
+    crew = models.ManyToManyField(Crew, blank=True, null=True, related_name='movies', verbose_name='Cast and Crew')
 
     def __str__(self):
 
@@ -51,11 +55,12 @@ class Role(models.Model):
 
 class Review(models.Model):
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviews', verbose_name='User')
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews', verbose_name='Movie')
     viewer_rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], verbose_name='Viewer Rating')
-    title = models.CharField(max_length=60)
-    comment = models.TextField(max_length=300)
+    title = models.CharField(max_length=60, blank=True)
+    comment = models.TextField(max_length=300, blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
