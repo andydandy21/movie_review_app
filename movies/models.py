92e48143b7ded2dd571889047838306ca763157d
Andrew Smith <a.smithb21@gmail.com>
2021-04-12 15:36:06 -0500

    changed related_name for CastAndCrew.movie

--- movies/models.py
+++ movies/models.py
@@ -50,7 +50,7 @@ class CastAndCrew(models.Model):
     
     role = models.CharField(max_length=200, verbose_name='Role')
     person = models.ForeignKey(People, on_delete=models.CASCADE, related_name='roles')
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='roles')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='crew')
 
     class Meta:
 
