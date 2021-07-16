1f4a93b96139f72eb38bad461122ff6b5c661012
Andrew Smith <a.smithb21@gmail.com>
2021-07-16 12:34:17 -0500

    added css

--- movies/forms.py
+++ movies/forms.py
@@ -17,4 +17,13 @@ class ReviewForm(forms.ModelForm):
     ]
         widgets = {
             'viewer_rating': forms.RadioSelect(choices=CHOICES),
+            'title': forms.TextInput(attrs={
+                'placeholder':'Title',
+                'class':'form-control'
+            }),
+            'comment': forms.Textarea(attrs={
+                'placeholder':'Enter a comment',
+                'class':'form-control',
+                'style':'height:8em;'
+            })
         }
