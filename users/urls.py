ccb56f8fc803a27548f7295216b332d1a09cab2f
Andrew Smith <a.smithb21@gmail.com>
2021-09-13 16:14:51 -0500

    added in convenient password reset for logged in users

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ProfileDetailView, EmailChangeView
+from .views import ProfileDetailView, EmailChangeView, LoggedInPasswordResetView
 
 
 urlpatterns = [
     path('profile/', ProfileDetailView.as_view(), name='user_profile'),
-    path('email_change/', EmailChangeView.as_view(), name='email_change')
+    path('email_change/', EmailChangeView.as_view(), name='email_change'),
+    path('logged_in_password_reset', LoggedInPasswordResetView.as_view(), name='logged_in_password_reset'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
-from users.forms import EmailChangeForm
+from allauth.account.forms import ResetPasswordForm
 from allauth.account.models import EmailAddress
+from django.contrib import messages
 from django.views.generic import FormView
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 from django.urls import reverse_lazy
 
 from .forms import EmailChangeForm
@@ -17,12 +18,15 @@ class EmailChangeView(FormView):
     form_class = EmailChangeForm
 
     def form_valid(self, form):
+
         user = form.cleaned_data['user']
         EmailAddress.objects.filter(user=user).exclude(primary=True).delete()
         form.save(self.request)
+        messages.add_message(self.request, messages.INFO, 'E-mail confirmation has been sent to the requested address.')
         return super(EmailChangeView, self).form_valid(form)
     
     def get_success_url(self):
+
         return reverse_lazy('user_profile')
 
     def get_form_kwargs(self):
@@ -30,3 +34,16 @@ class EmailChangeView(FormView):
         kwargs = super(EmailChangeView, self).get_form_kwargs()
         kwargs['user'] = self.request.user
         return kwargs
+
+class LoggedInPasswordResetView(FormView):
+
+    template_name="profile_detail.html"
+    form_class = ResetPasswordForm
+
+    def form_valid(self, form):
+        form.save(self.request)
+        messages.add_message(self.request, messages.INFO, 'Password reset request has been sent to your email')
+        return super(LoggedInPasswordResetView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('user_profile')
