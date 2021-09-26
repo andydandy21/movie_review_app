2b9caef79fbc3ee743f70e6568084565e753b381
Andrew Smith <a.smithb21@gmail.com>
2021-09-26 11:28:25 -0500

    added my heroku domain to ALLOWED_HOSTS

--- movie_review_app/settings.py
+++ movie_review_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['thereelreview.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
